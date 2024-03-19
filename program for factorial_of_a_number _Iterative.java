be196ff2c35f6a54c5bf0b6a00758f76f2b238e2
LatikaMukhi <141509340+LatikaMukhi@users.noreply.github.com>
2024-03-19 09:09:47 +0530

    Create program for factorial_of_a_number _Iterative.java

---
+++ program for factorial_of_a_number _Iterative.java
@@ -0,0 +1,18 @@
+
+    public static long factorial(int n)
+    {
+        long fact = 1;
+ 
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+ 
+        return fact;
+    }
+ 
+    public static void main(String[] args)
+    {
+        int n = 5;
+        System.out.println("The Factorial of " + n + " is " + factorial(n));
+    }
+}
